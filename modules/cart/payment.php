e1385fc91b6e8e13e45ba241174c24857046e56d
lannguyen <nguyenngoclan.7@gmail.com>
2013-11-20 14:37:22 +0700

    payment advance gui

--- modules/cart/payment.php
+++ modules/cart/payment.php
@@ -1,4 +1,4 @@
-<div class="center_title_bar">
+<!-- <div class="center_title_bar">
 	MỜI KHÁCH HÀNG ĐIỀN THÔNG TIN ĐỂ THỰC HIỆN THANH TOÁN
 </div>
 <br /><br />
@@ -44,4 +44,34 @@
 	</td>
 </tr>
 </table>
+</form> -->
+
+<form method="post" action="index.php?mod=cart&act=xl_payment" id="contact">
+	<div class="center_title_bar">MỜI KHÁCH HÀNG ĐIỀN THÔNG TIN ĐỂ THỰC HIỆN THANH TOÁN</div>
+	<div class="prod_box_big">
+		<div class="center_prod_box_big">
+			<div class="contact_form">
+				<div class="form_row">
+					<label class="contact"><strong>Tên khách hàng:</strong> </label> <input
+						type="text" name="fullname" class="contact_input" />
+				</div>
+				<div class="form_row">
+					<label class="contact"><strong>Email:</strong> </label> <input
+						type="text"  name="email" placeholder="Your email" class="contact_input" />
+				</div>
+				<div class="form_row">
+					<label class="contact"><strong>Số điện thoại:</strong> </label> <input
+						type="text" class="contact_input" name="tel" />
+				</div>
+				<div class="form_row">
+					<label class="contact"><strong>Địa chỉ:</strong> </label>
+					<input type="text" class="contact_input" name="add"/>
+				</div>
+				<div style="margin: 30px 0 20px 90px;">
+					<input type="submit" value="Thanh Toan" />
+					<input type="reset" value="Huy" />
+				</div>
+			</div>
+		</div>
+	</div>
 </form>
