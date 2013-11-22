c393c229e499b1e7129d7064821bb2fdd4c27f08
lannguyen <nguyenngoclan.7@gmail.com>
2013-11-22 16:28:25 +0700

    22-11-2103

--- demo/upload_file.php
+++ demo/upload_file.php
@@ -1,1 +1,58 @@
+<html>
+<body>
+
+<form action="upload.php" method="post"
+enctype="multipart/form-data">
+<label for="file">Filename:</label>
+<input type="file" name="file" id="file"><br>
+<input type="submit" name="submit" value="Submit">
+</form>
+
+</body>
+</html> 
+
 <?php
+$path = $_SERVER['DOCUMENT_ROOT'].'/demo/upload/';
+if (!file_exists("{$path}")) {
+    mkdir("{$path}");
+}
+
+$path = "/home/lan/Desktop/";
+
+$allowedExts = array("gif", "jpeg", "jpg", "png");
+$temp = explode(".", $_FILES["file"]["name"]);
+$extension = end($temp);
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/jpg")
+|| ($_FILES["file"]["type"] == "image/pjpeg")
+|| ($_FILES["file"]["type"] == "image/x-png")
+|| ($_FILES["file"]["type"] == "image/png"))
+&& in_array($extension, $allowedExts))
+  {
+  if ($_FILES["file"]["error"] > 0)
+    {
+    echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+    }
+  else
+    {
+    echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+    echo "Type: " . $_FILES["file"]["type"] . "<br>";
+    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+
+    if (file_exists("{$path}" . $_FILES["file"]["name"]))
+      {
+      echo $_FILES["file"]["name"] . " already exists. ";
+      }
+    else{
+      move_uploaded_file($_FILES["file"]["tmp_name"],
+      "{$path}" . $_FILES["file"]["name"]);
+      echo "Stored in: " . "{$path}" . $_FILES["file"]["name"];
+      }
+    }
+}
+else{
+	echo "Invalid file";
+}
+?> 
